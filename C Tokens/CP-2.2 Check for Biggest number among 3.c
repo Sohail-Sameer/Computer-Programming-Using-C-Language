7c8f3d351d0cfba792ad91f8205b01390376976b
Sohail-Sameer <72188612+Sohail-Sameer@users.noreply.github.com>
2025-12-30 13:03:19 +0530

    Implement logic to find the biggest of three numbers

--- C Tokens/CP-2.2 Check for Biggest number among 3.c
+++ C Tokens/CP-2.2 Check for Biggest number among 3.c
@@ -1,4 +1,44 @@
 #include <stdio.h>
-main(){
-  int a,b,c;
+int main(){
+  int a, b, c;
+  scanf("%d %d %d", &a, &b, &c);
+  if ((a==b)&&(b==c)){
+      printf("All numbers are equal");
+  }
+  else if ((a==b)||(b==c)||(c==a)){
+      if ((a>c)&&(a==b)){
+          printf("a and b are big");
+      }
+      else if ((c>a)&&(a==b)){
+          printf("c is big");
+      }
+      else if ((a>c)&&(b==c)){
+          printf("a is big");
+      }
+      else if ((c>a)&&(b==c)){
+          printf("b and c are big");
+      }
+      else if ((b>c)&&(c==a)){
+          printf("b is big");
+      }
+      else if ((c>b)&&(a==c)){
+          printf("a and c are big");
+      }
+  }
+  else{
+      if (a>b){
+          if (a>c){
+              printf("a is big");
+          }
+          else{
+              printf("c is big");
+          }
+      }
+      else if (b>c){
+          printf("b is big");
+      }
+      else{
+          printf("c is big");
+      }
+  }
 }
