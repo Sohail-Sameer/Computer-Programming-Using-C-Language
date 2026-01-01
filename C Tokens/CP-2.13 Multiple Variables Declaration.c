bd95f1139645408a60c45a42eaf34d3495bd79ad
Sohail-Sameer <72188612+Sohail-Sameer@users.noreply.github.com>
2026-01-01 22:04:10 +0530

    Add program to calculate volume of a box

---
+++ C Tokens/CP-2.13 Multiple Variables Declaration.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+  int length = 12, width = 8, height = 5;
+  int volume = length*width*height;
+
+  printf("Length: %d cm\n", length);
+  printf("Width: %d cm\n", width);
+  printf("Height: %d cm\n", height);
+  printf("Volume: %d cubic cm\n", volume);
+
+  return 0;
+}
